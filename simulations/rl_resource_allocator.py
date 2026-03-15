0ac34b74844c2ccea32831e8d86c78315613c215
sdjs-s-a-gh <scottberston@gmail.com>
2026-03-15 14:51:07 +0000

    entropy definitely works 2

--- simulations/rl_resource_allocator.py
+++ simulations/rl_resource_allocator.py
@@ -11,11 +11,11 @@ class RLResourceAllocator:
     def __init__(self, state_space_dimensions, action_space_dimensions) -> None:
         self.state_space_dimensions = state_space_dimensions
         self.action_space_dimensions = action_space_dimensions
-        
+
         # PPO Algorithm Step 1: Initialising the Policy (Actor) and Value (Critic) networks.
         self.policy_network = PolicyNetwork(self.state_space_dimensions, self.action_space_dimensions)
         self.value_network = ValueNetwork(self.state_space_dimensions)
-        
+
         # Default Hyperparameter Values
         self.batch_size = 512           # Number of timesteps per episode.
         self.updates_per_episode = 5    # Number of times to update the policy/actor and value/critic networks per episode.
@@ -23,28 +23,28 @@ class RLResourceAllocator:
         self.gamma = 0.95               # Discount factor to be used for cal
         self.clip_parameter = 0.2       # Value to clip the ratio when calculating surrogate 2.
         self.entropy_coefficient = 0.01 # Value to multiply the entropy loss by to encourage/disencourage exploration. The value is the same as that used in Mahimalmur (2025).
-        
-        
-        
+
+
+
         # Optimisers for more stable convergence
         self.policy_optimiser = optim.Adam(self.policy_network.parameters(), lr=self.learning_rate)
         self.value_optimiser = optim.Adam(self.value_network.parameters(), lr=self.learning_rate)
-        
+
         # Set a seed value for reproducible results.
         torch.manual_seed(1)
-        
+
         self.batch_actions = []
         self.batch_states = []
         self.batch_log_probabilities = []
         self.batch_rewards = []
-        
+
         # Try to load the Existing Policy and Value networks for future episodes.
         if Path("./ppo_policy.pth").is_file() and Path("./ppo_policy.pth").is_file():
             self.policy_network.load_state_dict(torch.load("ppo_policy.pth"))
             self.value_network.load_state_dict(torch.load("ppo_value.pth"))
             print("loaded .pth files")
-        
-        
+
+
     def get_action(self, state):
         """
             Returns the percentage of CPU that the resource allocator should give to the incoming task. This
@@ -71,17 +71,17 @@ class RLResourceAllocator:
 
         # Create a Gaussian distribution with the mean action and the standard deviation.
         distribution = Normal(mean, std)
-        
+
         # Sample an action from the distribution.
         action = distribution.sample()
-        
+
         # clamp the action to between 0, 1?
 
         # Calculate the log probability for that action to be successful.
         log_probability = distribution.log_prob(action)
 
         return action.detach(), log_probability.detach()     
-   
+
     def add_trajectory(self, action, log_probability, new_state, reward):
         """
             Adds the input trajectory to the current batch.
@@ -107,46 +107,47 @@ class RLResourceAllocator:
         batch_states = torch.tensor(self.batch_states, dtype=torch.float)
         batch_log_probablities = torch.tensor(self.batch_log_probabilities, dtype=torch.float)
         batch_rewards = torch.tensor(self.batch_rewards, dtype=torch.float)
-        
+
         # PPO Algorithm Step 4: Calculate Rewards to Go
         batch_rewards_to_go = self.compute_rewards_to_go(batch_rewards)
-        
+
         value = self.value_network(batch_states)
-        
+
         # PPO Algorithm Step 5: Calculate Advantages
         advantages = batch_rewards_to_go - value.detach()
-        
+
         # Normalise Advantages for improved stability.
         advantages = (advantages - advantages.mean() / (advantages.std() + 1e-10))
-        
+
         for epoch in range(self.updates_per_episode):
             # Calculate the Value and Current Log probabilities for the current epoch.
             value = self.value_network(batch_states)            
+            current_log_probabilities, _ = self.policy_network.evaluate(batch_states, batch_actions)
             current_log_probabilities, entropy = self.policy_network.evaluate(batch_states, batch_actions)
-            
+
             # Calculate ratios for the surrogate losses.
             ratios = torch.exp(current_log_probabilities - batch_log_probablities)
-            
+
             # Calculate Surrogate Losses.
             surr1 = ratios * advantages
             surr2 = torch.clamp(ratios, 1 - self.clip_parameter, 1 + self.clip_parameter) * advantages   
-            
+
             # PPO Algorithm Step 6: Update the Policy.
             policy_loss = (-torch.min(surr1, surr2)).mean()
-            policy_loss =- self.entropy_coefficient * entropy.mean()
-            
+            policy_loss -= self.entropy_coefficient * entropy.mean()
+
             self.policy_optimiser.zero_grad()
             policy_loss.backward(retain_graph = True)
             self.policy_optimiser.step()
-            
+
             # PPO Algorithm Step 7: Fit Value function by regression on MSE using the
             # predicted values at the current epoch.
             value_loss = nn.MSELoss()(value, batch_rewards_to_go)
-            
+
             self.value_optimiser.zero_grad()
             value_loss.backward()
             self.value_optimiser.step()
-            
+
         print("---- PPO Update Start ----")      
         print("Average reward:", sum(self.batch_rewards)/len(self.batch_rewards))
         print("Reward min/max:", min(self.batch_rewards), max(self.batch_rewards))
@@ -156,19 +157,19 @@ class RLResourceAllocator:
         print("Policy loss:", policy_loss.item())
         print("Value loss:", value_loss.item())
         print("---- PPO Update Complete ----")    
-            
+
     def compute_rewards_to_go(self, batch_rewards):
         batch_rewards_to_go = []
-        
+
         discounted_reward = 0
-        
+
         for reward in reversed(batch_rewards):
             discounted_reward = reward + discounted_reward * self.gamma
-                
+
             batch_rewards_to_go.insert(0, discounted_reward)
-        
+
         batch_rewards_to_go = torch.tensor(batch_rewards_to_go, dtype=torch.float)
-        
+
         return batch_rewards_to_go
 
 
@@ -177,7 +178,7 @@ class RLResourceAllocator:
         self.batch_log_probabilities = []
         self.batch_states = []
         self.batch_rewards = []
-        
+
 
     def update_and_save(self):
         """
@@ -185,15 +186,14 @@ class RLResourceAllocator:
         """
         # PPO Algorithm Step 2: Learn for some number of iterations. In this case,
         # an interation's length = to the batch size, which itself is equal to the episode length.
-        
-            
+
+
         # Update both the policy and value networks.
         self.learn()            
-            
+
         # Once the episode has ended, clear the batch to prepare for the new one.
         self.clear()
-            
+
         torch.save(self.policy_network.state_dict(), "./ppo_policy.pth")
         torch.save(self.value_network.state_dict(), "./ppo_value.pth")
-        print("The files have been saved.")           
-    
+        print("The files have been saved.")   
